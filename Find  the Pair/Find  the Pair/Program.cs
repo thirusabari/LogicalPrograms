07d381799ce4d7f0feab995d3c722fa9f16da778
Thirunavukkarasu G <thirunavukkarasu.gnanasekaran@syncfusion.com>
2018-06-02 20:20:26 +0530

    Find the Pair
     Lets Consider the a hard coded Collection. The Array of numbers are [9,3,1,5,7,10]
     You need to find if there exists a pair whose sum is equal to ther user's input.
    If any pair(two numbers) in a collection is equal to the input value, return the value as true. Otherwise return as false

---
+++ Find  the Pair/Find  the Pair/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Find__the_Pair
+{
+    // Lets Consider the a hard coded Collection. The Array of numbers are [9,3,1,5,7,10]
+    // You need to find if there exists a pair whose sum is equal to ther user's input.
+    // If any pair(two numbers) in a collection is equal to the input value, return the value as true. Otherwise return as false
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[] { 9, 3, 1, 5, 7, 10 };
+            int count = 0;
+            Console.WriteLine("Find the Pair");
+            Console.WriteLine("Enter the input value");
+            int input = Convert.ToInt32(Console.ReadLine());
+            for (int j=0;j<numbers.Length;j++)
+            {
+                for (int i = j+1; i < numbers.Length; i++)
+                {
+                    if (numbers[i] + numbers[j] == input && count == 0)
+                    {
+                        Console.WriteLine("The two values are = "+numbers[j]+", "+numbers[i]);
+                        count++;
+                        Console.WriteLine(true);
+                        break;
+                    }
+                }
+            }
+            if (count == 0)
+            {
+                Console.WriteLine(false);
+            }
+            Console.ReadLine();
+        }
+    }
+}
