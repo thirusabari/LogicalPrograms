08689bc3fe8d66a70f56788c2fa6e9632ac613a6
Creator <thirusabari@users.noreply.github.com>
2018-11-28 12:29:47 +0530

    Garland Words and Palindrome Checker

---
+++ Garland Words/Garland Words/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Garland_Words
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your garland words");
+            string word = Console.ReadLine();
+            char[] c = word.ToCharArray();
+            string temp = string.Empty;
+            var garlandDegree = 0;
+            foreach (char c1 in c)
+            {
+                temp = temp + c1;
+                if(temp==word[word.Length-1].ToString() && garlandDegree==0){
+                    garlandDegree = temp.Length;
+                }
+                int N = temp.Length;
+                if (N == 2)
+                {
+                    Console.WriteLine(N);
+                    Console.WriteLine(word.Length - 1);
+                    Console.WriteLine(word.Substring(N, word.Length-N));
+                    string deg = word.Substring(N,word.Length-N);
+                    for (int d = deg.Length - 1; d >= 0; d++)
+                    {
+                        
+                        string r = string.Empty;
+
+
+                    }
+                    char[] reverse = deg.ToCharArray();
+                    Array.Reverse(reverse);
+                    Console.WriteLine("The Reversed String is= ",reverse);
+                }
+                
+            }
+            Console.WriteLine(garlandDegree);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Palindrome Checker/Palindrome Checker/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Palindrome_Checker
+{
+    // To Know More about this question, click on the following link,https://learn.freecodecamp.org/javascript-algorithms-and-data-structures/javascript-algorithms-and-data-structures-projects/palindrome-checker/
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Palindrome Checker");
+            string palin = string.Empty;
+            palin = Console.ReadLine();
+
+        }
+    }
+}
